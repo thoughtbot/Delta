e6015ecb24790a09840f383155346d297f433a9f
Jake Craige <james.craige@gmail.com>
2015-11-13 09:31:50 -0500

    Initial Commit

---
+++ Flux/Flux.h
@@ -0,0 +1,19 @@
+//
+//  Flux.h
+//  Flux
+//
+//  Created by James Craige on 11/13/15.
+//  Copyright © 2015 thoughtbot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Flux.
+FOUNDATION_EXPORT double FluxVersionNumber;
+
+//! Project version string for Flux.
+FOUNDATION_EXPORT const unsigned char FluxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Flux/PublicHeader.h>
+
+
